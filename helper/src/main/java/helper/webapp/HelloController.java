0d5197e9025a4227dcbb7ccfc32937a36cbb32bf
Alfread <Alfread@Alfread-PC>
2016-06-05 14:01:32 +0800

    修改hellocontroller的url mapping,預設頁面

--- helper/src/main/java/helper/webapp/HelloController.java
+++ helper/src/main/java/helper/webapp/HelloController.java
@@ -2,13 +2,19 @@ package helper.webapp;
 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/hello")
+@RequestMapping(value = "/")
 public class HelloController {
 
 	
+	@RequestMapping(value = "/" , method = RequestMethod.GET)
+	public String defaultResponse(){
+		return "Hello , how are you?";
+		
+	}
 	
 	
 	@RequestMapping("/{name}")
